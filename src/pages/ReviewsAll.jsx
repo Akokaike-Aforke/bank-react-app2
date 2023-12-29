8a9938dc051e1c0a565f61b4db4bc38a24cd1e02
Akokaike-Aforke <lucykenneth54@gmail.com>
2023-12-29 14:23:13 +0100

    resolving bar chart issues

--- src/pages/ReviewsAll.jsx
+++ src/pages/ReviewsAll.jsx
@@ -461,7 +461,7 @@ const ReviewDiv = styled.main`
     display: flex;
     flex-direction: column;
     /* row-gap: 0.06rem; */
-    margin-top: 0.04rem;
+    /* margin-top: 0.04rem; */
     border: 1px solid black;
   }
   .star-span {
