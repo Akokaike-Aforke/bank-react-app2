454e163e92397e849b36a931b78a54567081f18a
Akokaike-Aforke <lucykenneth54@gmail.com>
2023-12-29 10:06:05 +0100

    resolving bar chart issues

--- src/pages/ReviewsAll.jsx
+++ src/pages/ReviewsAll.jsx
@@ -242,7 +242,7 @@ const ReviewsAll = () => {
   ));
 
   const  stats  = rating;
-  console.log(stats.groupTotals);
+  console.log(`groupTotals: ${stats.groupTotals}`);
   // const stat = stats[0];
   // console.log(stat);
   // console.log("rating.data");
