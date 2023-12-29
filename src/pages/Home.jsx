81870a1da85c4455e4ac42971663388293030f8c
Akokaike-Aforke <lucykenneth54@gmail.com>
2023-12-29 18:36:42 +0100

    resolving bar chart issues

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import logo from '../Images/Fidelity-Bank-Logo.png'
 import { FaBars } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
